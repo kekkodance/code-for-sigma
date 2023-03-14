67beb70a66b867260fb746bce94bf94b7586788a
kekkodance <53054786+kekkodance@users.noreply.github.com>
2023-03-14 23:16:45 +0100

    Update code.js

--- code.js
+++ code.js
@@ -2,16 +2,7 @@
 var style = document.createElement('style');
 
 // add the CSS as a string
-style.innerHTML = '.ui.bottom.right.wide.basic.popup.transition.visible.Popup_wrapper__gjV0g { display: none !important; }';
+style.innerHTML = '.ui.bottom.right.wide.basic.popup.transition.visible.Popup_wrapper__gjV0g { display: none !important; } .Header_logo__shVBB.Header_title__l\+wMf { visibility: hidden; } .Header_logo__shVBB.Header_title__l\+wMf:after { visibility: visible; position: absolute; top: 0; left: 1rem; content: "TheSigmaArts"; } ';
 
 // add it to the head
 document.getElementsByTagName('head')[0].appendChild(style);
-
-// select the element with the specified class
-const headerItem = document.querySelector('itemHeader_item__OOEY7.Header_itemHoverable__nf6CM');
-
-// add a click event listener to the element
-window.addEventListener('click', function() {
-  // open the website in a new window/tab
-  window.open('https://ripped.guide', '_blank');
-});
