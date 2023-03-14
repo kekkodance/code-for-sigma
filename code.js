c139879a2aa5e8e9f68b017ad20eed605c1d19ff
kekkodance <53054786+kekkodance@users.noreply.github.com>
2023-03-14 22:56:48 +0100

    chat gpt codes for me

--- code.js
+++ code.js
@@ -6,3 +6,12 @@ style.innerHTML = '.ui.bottom.right.wide.basic.popup.transition.visible.Popup_wr
 
 // add it to the head
 document.getElementsByTagName('head')[0].appendChild(style);
+
+// select the element with the specified class
+const headerItem = document.querySelector('itemHeader_item__OOEY7.Header_itemHoverable__nf6CM');
+
+// add a click event listener to the element
+headerItem.addEventListener('click', function() {
+  // open the website in a new window/tab
+  window.open('https://ripped.guide', '_blank');
+});
