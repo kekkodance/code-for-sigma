4570e0fb83afe789b9b6834e52ae273004f897ca
kekkodance <53054786+kekkodance@users.noreply.github.com>
2023-03-14 22:51:53 +0100

    Update code.js

--- code.js
+++ code.js
@@ -2,7 +2,7 @@
 var style = document.createElement('style');
 
 // add the CSS as a string
-style.innerHTML = '.ui.horizontal.section.divider { display: none; }';
+style.innerHTML = '.ui.bottom.right.wide.basic.popup.transition.visible.Popup_wrapper__gjV0g { display: none !important; }';
 
 // add it to the head
 document.getElementsByTagName('head')[0].appendChild(style);
