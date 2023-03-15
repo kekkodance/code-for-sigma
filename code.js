194cb27ddf1afe6abd840b5cdee0a05a435205f6
kekkodance <53054786+kekkodance@users.noreply.github.com>
2023-03-15 13:16:31 +0100

    Update code.js

--- code.js
+++ code.js
@@ -2,7 +2,22 @@
 var style = document.createElement('style');
 
 // add the CSS as a string
-style.innerHTML = '.ui.bottom.right.wide.basic.popup.transition.visible.Popup_wrapper__gjV0g { display: none !important; } .Header_logo__shVBB.Header_title__l+\wMf { visibility: hidden; } .Header_logo__shVBB.Header_title__l+\wMf:after { visibility: visible; position: absolute; top: 0; left: 1rem; content: "TheSigmaArts"; } ';
+style.innerHTML = '.Header_logo__shVBB {
+ visibility: hidden;
+}
+.Header_logo__shVBB:after {
+ visibility: visible;
+ position: absolute;
+ top: 0;
+ left: 1rem;
+ content: "TheSigmaArts";
+}
+a.item:nth-child(2) {
+  user-select: none !important;
+}
+#app > div:nth-child(3) {
+  display: none !important;
+}';
 
 // add it to the head
 document.getElementsByTagName('head')[0].appendChild(style);
